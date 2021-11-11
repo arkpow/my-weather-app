c59c391521b7c168920a8113c9b0831a8b18e9ce
arkpow <alkerpower@gmail.com>
2021-11-11 14:43:31 +0300

    hi

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { uniqueId } from "lodash";
 import classes from './SearchList.module.css'
 
 export const SearchList = ({ onSelectCity, citiesArray }) => {
   return (
     <ul className={classes.ul}>
-      {citiesArray.map((item) => <li onClick={() => onSelectCity(item)} className={classes.li} key={uniqueId()} ><b>{item.city}</b>, {item.state}</li>)}
+      {citiesArray.map((item, index) => <li onClick={() => onSelectCity(item)} className={classes.li} key={index} ><b>{item.city}</b>, {item.state}</li>)}
     </ul>
   )
 }
